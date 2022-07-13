0b103987d6ab20dafeb1881238e51c9395831356
Quantee <quantee.alt@pm.me>
2022-07-13 15:13:51 +0200

    Implement the API to get data.

---
+++ api.py
@@ -0,0 +1,69 @@
+import flask
+import pathlib
+from monitor import get_monitor_version
+from data_load import load_map_data, load_server_data
+
+API_VERSION = 1
+MONITOR_SERVERS = ["Overcast Community"]  # Folders inside /save/ from where the API can serve data.
+
+
+def has_access(server):
+    if server in MONITOR_SERVERS: return True
+    else: return False
+
+
+app = flask.Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return flask.jsonify({"api_version": API_VERSION,
+                          "monitor_version": get_monitor_version(),
+                          "monitored_server_count": len(MONITOR_SERVERS),
+                          "monitored_servers": MONITOR_SERVERS})
+
+
+@app.route("/<string:server_name>/", methods=["GET"])
+def server_data(server_name):
+    if not has_access(server_name): return flask.jsonify("Requested server not found or forbidden"), 403
+
+    directory = pathlib.Path(f"save/{server_name}")
+
+    if directory.is_dir():
+        monitoring_since, last_cache, maps_tracked = load_server_data(directory)
+        # Return response
+        return flask.jsonify({"name": server_name,
+                              "monitoring_since": monitoring_since,
+                              "last_cache_update": last_cache,
+                              "maps_tracked": maps_tracked})
+    else:
+        return flask.jsonify("Requested server not found"), 404
+
+
+@app.route("/<string:server_name>/maps/<string:map_name>", methods=["GET"])
+def map_data(server_name, map_name):
+    if not has_access(server_name): return flask.jsonify("Requested server not found or forbidden"), 403
+
+    directory = pathlib.Path(f"save/{server_name}")
+
+    if directory.is_dir():
+        # Load data for map
+        is_found, cached_data_found, map_playcount, \
+        map_avg_playtime, map_avg_playercount_change = load_map_data(directory, map_name)
+
+        if not is_found:
+            return flask.jsonify("Requested map not found"), 404
+        else:
+            return flask.jsonify({"server_name": server_name,
+                                  "map_name": map_name,
+                                  "found_in_cache": cached_data_found,
+                                  "playcount": map_playcount,
+                                  "map_avg_playtime": map_avg_playtime,
+                                  "map_avg_playercount_change": map_avg_playercount_change})
+
+    else:
+        return flask.jsonify("Requested server not found"), 404
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=7000)
---
+++ data_load.py
@@ -0,0 +1,54 @@
+def load_map_data(directory, map_name):
+    is_found = False
+    with open((directory / "map_data"), "r") as file:
+        data = file.read()
+
+        map_playcount = 0
+        for line in data.splitlines():
+            split = line.split(" | ")
+            mapname_to_check = split[0]
+            playcount = int(split[1])
+
+            if mapname_to_check == map_name:
+                # The requested map exists
+                is_found = True
+                map_playcount = playcount
+                break
+
+    # Load cached data, if any
+    cached_data_found = False
+    with open((directory / "map_average_cache"), "r") as file:
+        data = file.read()
+
+        map_avg_playtime = 0
+        map_avg_playercount_change = 0
+        for line in data.splitlines():
+            split = line.split(" | ")
+            mapname = split[0]
+            playtime = float(split[1])
+            pcount_change = float(split[2])
+
+            if mapname_to_check == mapname:
+                # The requested map exists
+                cached_data_found = True
+                map_avg_playtime = playtime
+                map_avg_playercount_change = pcount_change
+                break
+
+    return is_found, cached_data_found, map_playcount, map_avg_playtime, map_avg_playercount_change
+
+
+def load_server_data(directory):
+    # Read files
+    with open((directory / "first_write"), "r") as file:
+        monitoring_since = file.read()
+    with open((directory / "last_cache_time"), "r") as file:
+        last_cache = file.read()
+    # Count number of maps in file
+    invalid = 0
+    with open((directory / "map_data"), 'r') as fp:
+        for count, line in enumerate(fp):
+            if line.startswith("SYS_"): invalid += 1
+    maps_tracked = count + 1 - invalid
+
+    return monitoring_since, last_cache, maps_tracked
