c851018c9f25057f6982c9c5ac75fb08ca3d76bb
Quantee <quantee.alt@pm.me>
2022-07-14 21:11:47 +0200

    Remove trailing slash from routes

--- flaskapp.py
+++ flaskapp.py
@@ -26,7 +26,7 @@ def index():
                           "monitored_servers": MONITOR_SERVERS})
 
 
-@app.route("/<string:server_name>/", methods=["GET"])
+@app.route("/<string:server_name>", methods=["GET"])
 def server_data(server_name):
     """
     Return information about a tracked server.
@@ -48,7 +48,7 @@ def server_data(server_name):
         return flask.jsonify("Requested server not found"), 404
 
 
-@app.route("/<string:server_name>/current_map/")
+@app.route("/<string:server_name>/current_map")
 def current_map(server_name):
     """
     Returns the currently playing map
