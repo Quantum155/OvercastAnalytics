4e73bb2fd6fe84cf85d134fe35784e98c4bf405d
quanteey <61941975+Quantum155@users.noreply.github.com>
2023-02-11 23:44:15 +0100

    Add get map data comment

---
+++ extensions/OvercastNotify/src/cogs/get_map_data.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+import requests
+from src.cogs.current_map import format_seconds  # noqa
+
+
+class MapData(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @discord.slash_command()
+    async def get_map_data(self, interaction: discord.Interaction, map_name: str):
+        api_response = requests.get(f"https://quanteey.xyz/Overcast%20Community/maps/{map_name}")
+        found_in_cache = api_response.json()["found_in_cache"]
+        playcount = api_response.json()["playcount"]
+        map_avg_playtime = api_response.json()["map_avg_playtime"]
+        map_avg_playercount_change = api_response.json()["map_avg_playercount_change"]
+        if api_response.status_code == 200:
+            await interaction.response.send_message(
+                f"Data for map: **{map_name}**\n"
+                f"Is cached: **{found_in_cache}**\n"
+                f"Times played: **{playcount}**\n"
+                f"Average playtime: **{format_seconds(map_avg_playtime)}**\n"
+                f"Average playercount change: **{map_avg_playercount_change}**")
+        else:
+            await interaction.response.send_message(
+                f"Quanteey API is not online / failed:"
+                f" error **{api_response.status_code}**")
+
+
+def setup(bot):
+    bot.add_cog(MapData(bot))
