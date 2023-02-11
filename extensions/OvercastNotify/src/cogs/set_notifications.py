e0cdf23db2dd8dce9d3c65062dd3b610dfb03426
quanteey <61941975+Quantum155@users.noreply.github.com>
2023-02-11 14:02:56 +0100

    WIP: Notify when a long game has been going on

--- extensions/OvercastNotify/src/cogs/set_notifications.py
+++ extensions/OvercastNotify/src/cogs/set_notifications.py
@@ -12,21 +12,22 @@ class SetNotifications(commands.Cog):
         self.config.touch()
 
     @discord.slash_command()
-    async def set_notifications(self, interaction: discord.Interaction, group: discord.Role, after: int, repeat_every: int):
+    async def set_notifications(self, interaction: discord.Interaction, group: discord.Role, after: int, message: str):
         """
         Set notification settings for a server.
         :param interaction: (Provided by discord)
         :param group: Discord role that will be mentioned
         :param after: Seconds of gametime after which notifications should be sent
-        :param repeat_every: After the first notification, repeat every X seconds
         """
         with open(self.config, "r") as file:
             configs = json.load(file)
 
-        configs[interaction.guild_id] = {"group": group.id, "after": after, "repeat": repeat_every}
+        configs[interaction.guild_id] = {"group": group.id, "after": after, "channel": interaction.channel.id, "message": message}
 
         with open(self.config, "w") as file:
             json.dump(configs, file, indent=4)
+
+        await interaction.response.send_message(f"Set notification settings for **{interaction.guild.name}**")
 
 def setup(bot):
     bot.add_cog(SetNotifications(bot))
